4c03a5f6311844d4f8456ade231b19c2c547af39
Teyir <60007549+Teyir@users.noreply.github.com>
2021-09-07 16:40:21 +0200

    [IMPROVE] Avancement page live

--- pages/live.php
+++ pages/live.php
@@ -24,15 +24,33 @@
         <div class="col-lg-8">
             <!-- Score -->
             <div class="mb-4 text-center">
-                <h1><strong class="text-info">4</strong> - <strong class="text-secondary">8</strong></h1>
+
 
             </div>
 
             <!-- Fenêtre live streaming -->
-            <div class="card mb-10 text-center">
+            <div class="card mb-10 text-center overlay-content">
 
-                LIVE
-                <div style="background-color: #6f42c1; height: 400px"></div>
+                    <!-- Flux vidéo: http://192.168.43.235:8000/stream.mjpg -->
+                <img src="assets/img/placeholder_stream.png">
+
+                <div class="overlay-background-top">
+                    <h1><strong class="text-info">4</strong> - <strong class="text-secondary">8</strong></h1>
+                </div>
+
+                <!-- Joueur 1-->
+                <div class="overlay-block-j1">
+                    <img class="overlay-photo-profile" src="assets/img/logo_placeholder.png"><br>
+                    <span class="overlay-j1-pseudo">JOUEUR 1</span>
+                </div>
+
+                <!-- Joueur 2-->
+                <div class="overlay-block-j2">
+                    <img class="overlay-photo-profile" src="assets/img/logo_placeholder.png"><br>
+                    <span class="overlay-j2-pseudo">JOUEUR 2</span>
+                </div>
+
+
 
             </div>
 
