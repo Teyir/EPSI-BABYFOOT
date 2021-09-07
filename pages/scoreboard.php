0d578c0e4fa9024b645430e9c1f93dc075ec5e55
Mathurin <mathurinratie3003@gmail.com>
2021-09-07 15:57:27 +0200

    pseudos sur le scoreboard

--- pages/scoreboard.php
+++ pages/scoreboard.php
@@ -9,9 +9,21 @@
         <th scope="col">Score</th>
     </tr>
     </thead>
-    <tbody>²
+    <tbody>
     <!--  Vérification de participation au match, de victoire et de défaite :  -->
     <?php foreach ($historique_profile as $historique):
+
+        //Affichage du pseudo du joueur 1
+        $requete = $pdo->prepare("SELECT pseudo FROM users WHERE id=:id_j1");
+        $requete->bindParam(":id_j1", $historique['j1']);
+        $requete->execute();
+        $pseudo_id1 = $requete->fetch();
+
+        //Affichage du pseudo du joueur 1
+        $requete = $pdo->prepare("SELECT pseudo FROM users WHERE id=:id_j2");
+        $requete->bindParam(":id_j2", $historique['j2']);
+        $requete->execute();
+        $pseudo_id2 = $requete->fetch();
         ?>
         <tr
         <?php
@@ -41,8 +53,8 @@
         >
             <!--  Statistiques du match :  -->
             <th scope="row"><?= $historique['id']?></th>
-            <td><?= $historique['j1'] ?></td>
-            <td><?= $historique['j2'] ?></td>
+            <td><?= $pseudo_id1[0] ?></td>
+            <td><?= $pseudo_id2[0] ?></td>
             <td><?= $historique['score_j1'] ?> : <?= $historique['score_j2'] ?></td>
         </tr>
 
