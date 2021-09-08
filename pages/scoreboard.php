637bcbac70903b905a4dadb99700efd14b973408
Teyir <60007549+Teyir@users.noreply.github.com>
2021-09-08 09:24:41 +0200

    [IMPROVE] scoreboard.php & couleurs profile.php (Mathurin)

--- pages/scoreboard.php
+++ pages/scoreboard.php
@@ -1,1 +1,52 @@
-<?php
+<h2>Historique des parties</h2>
+<hr>
+<table class="table">
+    <thead>
+    <tr>
+        <th scope="col">#</th>
+        <th scope="col">Joueur 1</th>
+        <th scope="col">Joueur 2</th>
+        <th scope="col">Score</th>
+    </tr>
+    </thead>
+    <tbody>²
+    <!--  Vérification de participation au match, de victoire et de défaite :  -->
+    <?php foreach ($historique_profile as $historique):
+        ?>
+        <tr
+            <?php
+            if ($_SESSION["id_connecté"] == $historique['j1']){
+                if ($historique['score_j1'] > $historique['score_j2']) {
+                    ?>
+                    class="table-warning"
+                    <?php
+                }
+                else {
+                    ?>
+                    class="table-danger"
+                    <?php
+                }}
+            if ($_SESSION["id_connecté"] == $historique['j2']){
+                if ($historique['score_j2'] > $historique['score_j1']) {
+                    ?>
+                    class="table-warning"
+                    <?php
+                }
+                else {
+                    ?>
+                    class="table-danger"
+                    <?php
+                }}
+            ?>
+        >
+            <!--  Statistiques du match :  -->
+            <th scope="row"><?= $historique['id']?></th>
+            <td><?= $historique['j1'] ?></td>
+            <td><?= $historique['j2'] ?></td>
+            <td><?= $historique['score_j1'] ?> : <?= $historique['score_j2'] ?></td>
+        </tr>
+
+    <?php endforeach; ?>
+
+    </tbody>
+</table>
